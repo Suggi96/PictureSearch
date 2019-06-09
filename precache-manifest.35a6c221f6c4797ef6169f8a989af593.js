6d96e224b317cd16ce1f0962bee350312498aa1e
Sugandh <emsugandh@gmail.com>
2019-06-09 18:01:42 +0530

    Updates

---
+++ precache-manifest.35a6c221f6c4797ef6169f8a989af593.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13e79beb1728d92b384703f689925ad0",
+    "url": "/PictureSearch/index.html"
+  },
+  {
+    "revision": "8481da4c70d45c46b9e9",
+    "url": "/PictureSearch/static/css/main.215eff9a.chunk.css"
+  },
+  {
+    "revision": "01f001c735625cba4d20",
+    "url": "/PictureSearch/static/js/2.fc36bde1.chunk.js"
+  },
+  {
+    "revision": "8481da4c70d45c46b9e9",
+    "url": "/PictureSearch/static/js/main.bf1a2bd2.chunk.js"
+  },
+  {
+    "revision": "01008d476ece1f06e599",
+    "url": "/PictureSearch/static/js/runtime~main.ee893c60.js"
+  }
+]);
